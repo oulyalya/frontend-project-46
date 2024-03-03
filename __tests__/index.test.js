da0899f738d915cbbafb2b917a3d5e1923a75fc0
Olga <olya.debelaya@gmail.com>
2024-03-03 21:02:27 +0300

    ref: code climate duplication issue

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -16,9 +16,9 @@ const stylishDiffFilename = 'res_file1-file2_stylish.txt';
 const plainDiffFilename = 'res_file1-file2_plain.txt';
 const jsonDiffFilename = 'res_file1-file2_json.txt';
 
-const stylishDiffColoredFilename = 'res_file1-file2_stylish_color.txt';
-const plainDiffColoredFilename = 'res_file1-file2_plain_color.txt';
-const jsonDiffColoredFilename = 'res_file1-file2_json_color.txt';
+const stylishDiffColoredFilename = 'res_file1-file2_stylish_colored.txt';
+const plainDiffColoredFilename = 'res_file1-file2_plain_colored.txt';
+const jsonDiffColoredFilename = 'res_file1-file2_json_colored.txt';
 
 const getFixturePath = (filename) => path.resolve(process.cwd(), '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
@@ -37,20 +37,24 @@ test.each([
 });
 
 test.skip.each([
-  [jsonFilename1, jsonFilename2, 'stylish', stylishDiffColoredFilename],
-  [yamlFilename1, yamlFilename2, 'stylish', stylishDiffColoredFilename],
+  // [jsonFilename1, jsonFilename2, 'stylish', stylishDiffColoredFilename],
+  // [yamlFilename1, yamlFilename2, 'stylish', stylishDiffColoredFilename],
   [jsonFilename1, yamlFilename2, 'plain', plainDiffColoredFilename],
   [yamlFilename1, jsonFilename2, 'plain', plainDiffColoredFilename],
-])('test color coded getFilesDiff(%s, %s) "%s"', (filename1, filename2, formatName, diffFilename, isColored) => {
-  const actual = true;
-  const expected = true;
+])('test color coded getFilesDiff(%s, %s) "%s"', (filename1, filename2, formatName, diffFilename, isColored = true) => {
+  const expected = readFile(diffFilename).trim();
+  const actual = getFilesDiff(
+    getFixturePath(filename1),
+    getFixturePath(filename2),
+    formatName,
+    isColored,
+  ).trim();
 
   expect(actual).toBe(expected);
 });
 
 test.each([
   [jsonFilename1, jsonFilename2, 'json', jsonDiffFilename],
-  [yamlFilename1, yamlFilename2, 'json', jsonDiffFilename],
   [jsonFilename1, yamlFilename2, 'json', jsonDiffFilename],
   [yamlFilename1, jsonFilename2, 'json', jsonDiffFilename],
 ])('test getFilesDiff(%s, %s) "%s"', (filename1, filename2, formatName, diffFilename) => {
